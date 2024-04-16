53e9393b38a690ead3495f7425e72fe049988295
vitekvn01 <vitekvn01@yandex.ru>
2024-04-16 14:40:53 +0700

    Added dialogue system, interaction and UI

---
+++ Assets/Scripts/SingletonBase/SingletonBase.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public abstract class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour
+{
+    [Header("Singleton")]
+    [SerializeField] private bool _doNotDestroyOnLoad;
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("MonoSingleton: object of type already extist, instance will be destroyed = " + typeof(T).Name);
+            Destroy(this);
+            return;
+        }
+
+        Instance = this as T;
+
+        if (_doNotDestroyOnLoad)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/_base/player/Interaction.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private float _length;
+
+    [SerializeField] private GameObject _playerView;
+
+    [SerializeField] private GameObject _panelInteraction;
+
+    private bool _interaction = false;
+    void Update()
+    {
+        if (_interaction == true)
+        {
+            _panelInteraction.SetActive(true);
+        }
+        else _panelInteraction.SetActive(false);
+
+        if (Physics.Raycast(_playerView.transform.position, _playerView.transform.forward, out RaycastHit hit))
+        {
+            if (hit.collider != null && hit.collider.GetComponent<DialogueCharacter>() != null)
+            {
+                DialogueCharacter character = hit.collider.GetComponent<DialogueCharacter>();
+                _interaction = true;
+
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    Debug.Log("взаимодейсвие произошло с" + hit.collider.name);
+                    character.StartDialogue();
+                    _panelInteraction.SetActive(false);
+                }
+            }
+            else _interaction = false;
+        }
+        else _interaction = false;
+    }
+}
+
+
+
+
