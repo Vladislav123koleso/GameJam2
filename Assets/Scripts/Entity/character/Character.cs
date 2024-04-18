920f6bffbab6d669a613c6719fa7a3d1a05baf52
vitekvn01 <vitekvn01@yandex.ru>
2024-04-18 23:08:05 +0700

    Revert "fix movement player"
    This reverts commit 01cec3ef171c15cba1417d274e19cd577e4c255f.

--- Assets/Scripts/Entity/character/Character.cs
+++ Assets/Scripts/Entity/character/Character.cs
@@ -15,11 +15,4 @@ public class Character : MonoBehaviour
     [SerializeField] private string _name;
     public string Nickname => _name;
 
-    private Animator animator;
-
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-        animator.Play("jump");
-    }
 }
